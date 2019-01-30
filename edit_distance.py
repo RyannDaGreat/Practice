73d9ca7dd39b4c75ecb71deee4bb7c6aff376915
RyanBurgert219 <sqrtryan@gmail.com>
2019-01-30 04:28:50 -0500

    aoisd

--- edit_distance.py
+++ edit_distance.py
@@ -1,12 +1,13 @@
 def edit_distance(start,end):
+    from functools import lru_cache
     @lru_cache(None,None)
     def helper(start,end,dist):
-        if not(start or end):return dist
-        if bool(start)!=bool(end):return len(start or end)+dist
-        if start[0]==end[0]:return helper(start[1:],end[1:],dist+0)
-        else:return min(helper(start[1:],end    ,dist+1),#Delete
-                        helper(start    ,end[1:],dist+1),#Insert
-                        helper(start[1:],end[1:],dist+1))#Replace
+        if not start and not end   :return                                dist  #Base
+        if not start or  not end   :return len(       start or  end)     +dist  #Failure
+        if     start[0]  ==  end[0]:return     helper(start[1:],end[1:],  dist) #Skip
+        else:                       return min(helper(start[1:],end    ,1+dist),#Delete
+                                               helper(start    ,end[1:],1+dist),#Insert
+                                               helper(start[1:],end[1:],1+dist))#Replace
     return helper(start,end,0)
 print(edit_distance('kitt','mitt'))#1
 print(edit_distance('1234','abcd'))#4
